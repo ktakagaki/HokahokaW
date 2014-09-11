8065b42806b8ff7d884bd81cd5dec469ee83cdb6
ktakagaki <kentaroh.takagaki@gmail.com>
2014-09-12 01:58:15 +0200

    Finished git printing functions, test, documentation

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,15 @@
 (* Paclet Info File *)
 
-(* created 2014.09.11*)
+(* created 2014/09/12*)
 
 Paclet[
-  Name -> "HokahokaW",
-  Version -> "0.0.1",
-  MathematicaVersion -> "6+",
-  Extensions -> {
-    {"Documentation", Language -> "English"}
-}]
+    Name -> "HokahokaW",
+    Version -> "0.0.1",
+    MathematicaVersion -> "9+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
