385f2f0237f927eb888b843acae24bbab7179198
Kenta Takagaki <kentaroh.takagaki@gmail.com>
2015-02-01 21:25:08 +0100

    Basic implementations copied with untested parts commented out.

--- HokahokaW/Kernel/init.m
+++ HokahokaW/Kernel/init.m
@@ -1,3 +1,8 @@
 (* Wolfram Language Init File *)
 
-Get[ "HokahokaW`HokahokaW`"]
+Get[ "HokahokaW`HokahokaW`"];
+Get[ "HokahokaW`Package`"];
+Get[ "HokahokaW`Rules`"];
+Get[ "HokahokaW`Java`"];
+Get[ "HokahokaW`Graphics`"];
+Get[ "HokahokaW`Signal`"];
